b4c9e5604efbbcbf3337a81a1617f2ff984f32da
David P. Bauer <dbauer1@udayton.edu>
2015-12-17 09:14:30 -0500

    SAKAI-2168 Only accept microsoft word compatible files

--- api/src/java/org/sakaiproject/ddo/model/SubmissionFile.java
+++ api/src/java/org/sakaiproject/ddo/model/SubmissionFile.java
@@ -20,11 +20,7 @@ public class SubmissionFile {
     // Only accept files that are Microsoft Office and do not have macros enabled.
     public static final String[] AcceptableMimeTypes = new String[]{
             "application/msword",
-            "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-            "application/vnd.ms-excel",
-            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-            "application/vnd.ms-powerpoint",
-            "application/vnd.openxmlformats-officedocument.presentationml.presentation"
+            "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
     };
 
     private byte[] bytes;
