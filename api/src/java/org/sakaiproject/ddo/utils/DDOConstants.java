39f0310dcf20634ba353d5f9e699a69542b5273b
David P. Bauer <davidpbauer@users.noreply.github.com>
2017-03-20 09:53:49 -0400

    Use query string for date params instead of url segments (#11)

--- api/src/java/org/sakaiproject/ddo/utils/DDOConstants.java
+++ api/src/java/org/sakaiproject/ddo/utils/DDOConstants.java
@@ -27,4 +27,7 @@ public class DDOConstants {
 
     public static final String PROP_CLOSED = "closed";
     public static final String PROP_CLOSED_MESSAGE = "closed_message";
+
+    public static final String PARAM_START_DATE = "startDate";
+    public static final String PARAM_END_DATE = "endDate";
 }
